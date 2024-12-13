c01ddf7b7fcfd32833ab3dbb2fb26433439d2c9c
yuanguolei <yuanguolei@cnrmall.com>
2024-12-13 16:54:45 +0800

    zxing生成二维码条形码

--- READEME.md
+++ READEME.md
@@ -1,3 +1,5 @@
 ### spring boot：2.5.5
 ### redis：2.6 +
 ### swagger：3.0
+
+### -Djasypt.encryptor.password=
---
+++ src/main/java/com/y/gui/common/utils/ContentUtil.java
@@ -0,0 +1,6 @@
+package com.y.gui.common.utils;
+
+public class ContentUtil {
+    public static final String EMPTY_STR = "";
+    public static final String DOT = ".";
+}
